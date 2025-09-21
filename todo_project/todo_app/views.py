6b9ab09cc31d74da89d06766c05eee85045b089b
Charry Mae Atamosa <charrymaeatamosa@gmail.com>
2025-09-21 15:02:33 +0800

    feature(todo_app): complete app setup

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def home(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'home.html', {'tasks': tasks, 'form': form})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('home')
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('home')
